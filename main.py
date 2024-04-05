3ebe6a23e1545ecfb9e32027dde1512d4a147f07
FabricioVera <fabriciovera7@gmail.com>
2024-04-05 16:23:57 -0300

    ar

--- main.py
+++ main.py
@@ -18,15 +18,17 @@ from mqtt_as import MQTTClient
 from mqtt_local import config
 import uasyncio as asyncio
 import dht, machine
+from machine import Timer
 from collections import OrderedDict
-import ujson as json
+import json
 import ubinascii
 import btree
 import time
-from machine import Timer
 import settings
 
 CLIENT_ID = ubinascii.hexlify(machine.unique_id()).decode('utf-8')
+
+# configuracion de pines... Sensor, rele, led
 d = dht.DHT22(machine.Pin(13))
 pin_rele = dht.DHT22(machine.Pin(36))
 pin_led = dht.DHT22(machine.Pin(2))
@@ -34,10 +36,10 @@ pin_led = dht.DHT22(machine.Pin(2))
 
 setpoint=40
 #modo automatico = TRUE, MANUAL = FALSE
-modo=True
+modo=False
 #periodo en ms
-periodo=30000
-#rele empieza apagado
+periodo=3000
+#rele empieza apagado, es activo en bajo
 rele_estado=True
 datos={}
 
@@ -63,26 +65,52 @@ def transmitir(pin):
     client.publish(f"CLIENT_ID",datos, qos = 1)
     client.disconnect()
 
+timer_publicar=Timer(0)
+timer_publicar.init(period=periodo, mode=Timer.PERIODIC, callback=transmitir)
+
+
 def sub_cb(topic, msg, retained):
     print('Topic = {} -> Valor = {}'.format(topic.decode(), msg.decode()))
+    if topic.decode() == f"{CLIENT_ID}/periodo":
+            periodo = int(msg)
+            timer_publicar=Timer(0)
+            timer_publicar.init(period=periodo, mode=Timer.PERIODIC, callback=transmitir)
+        
+    if topic.decode() == f"{CLIENT_ID}/setpoint":
+        setpoint = int(msg)
+
+    if topic.decode() == f"{CLIENT_ID}/modo":
+        if msg == "auto":
+            modo = True
+        elif msg == "manual":
+            modo = False
+        print(f"modo = {msg}")
+
+    if topic.decode() == f"{CLIENT_ID}/rele":
+        if msg == "on":
+            rele_estado = False
+        elif msg == "off":
+            rele_estado = True
+
+    if topic.decode() == f"{CLIENT_ID}/destello":
+        if msg == "on":
+            destello()
 
 async def wifi_han(state):
     print('Wifi is ', 'up' if state else 'down')
     await asyncio.sleep(1)
 
-# If you connect with clean_session True, must re-subscribe (MQTT spec 3.1.2.4)
 async def conn_han(client):
-    await client.subscribe(f"CLIENT_ID", 1)
+    await client.subscribe(f'{CLIENT_ID}/temperatura', 1)  
+    await client.subscribe(f'{CLIENT_ID}/humedad', 1)  
+    await client.subscribe(f'{CLIENT_ID}/rele', 1)  
+    await client.subscribe(f'{CLIENT_ID}/modo', 1) 
+    await client.subscribe(f'{CLIENT_ID}/periodo', 1)
+    await client.subscribe(f'{CLIENT_ID}/destello', 1) 
 
 async def main(client):
-    await client.connect()
-
-    for coroutine in (up, messages):
-        asyncio.create_task(coroutine(client))
 
     await asyncio.sleep(2)  # Give broker time
-
-    await client.publish(f"CLIENT_ID","inciiando", qos = 1)
     
     while True:
         try:
@@ -98,7 +126,8 @@ async def main(client):
                 ('setpoint', setpoint)
                 ]))
             print(datos)
-            await client.publish(CLIENT_ID,"hola")
+            db[b"Temperatura"] = str(temperatura)
+            db[b"Humedad"] = str(humedad)
         except OSError as e:
             print("sin sensor temperatura")
         if modo == True:
@@ -109,62 +138,16 @@ async def main(client):
 
         pin_rele.value(rele_estado)
 
-        db[b"Temperatura"] = str(temperatura)
-        db[b"Humedad"] = str(humedad)
         db[b"Rele Estado"] = str(rele_estado)
         db.flush()
         await asyncio.sleep(180)  # Broker is slow
 
-timer_publicar=Timer(0)
-timer_publicar.init(period=periodo, mode=Timer.PERIODIC, callback=transmitir)
-
-
-
-async def up(client):  # Respond to connectivity being (re)established
-    while True:
-        await client.up.wait()  # Wait on an Event
-        client.up.clear()
-        await client.subscribe(f'{CLIENT_ID}/temperatura', 1)  # renew subscriptions
-        await client.subscribe(f'{CLIENT_ID}/humedad', 1)  # renew subscriptions
-        await client.subscribe(f'{CLIENT_ID}/rele', 1)  # renew subscriptions
-        await client.subscribe(f'{CLIENT_ID}/modo', 1)  # renew subscriptions
-        await client.subscribe(f'{CLIENT_ID}/periodo', 1)  # renew subscriptions
-        await client.subscribe(f'{CLIENT_ID}/destello', 1)  # renew subscriptions
-
-async def messages():
-    async for topic, msg, retained in client.queue:
-        if topic == f"{CLIENT_ID}/periodo":
-            periodo = int(msg)
-            timer_publicar=Timer(0)
-            timer_publicar.init(period=periodo, mode=Timer.PERIODIC, callback=transmitir)
-        
-        if topic == f"{CLIENT_ID}/setpoint":
-            setpoint = int(msg)
-
-        if topic == f"{CLIENT_ID}/modo":
-            if msg == "auto":
-                modo = True
-            elif msg == "manual":
-                modo = False
-            print(f"modo = {msg}")
-
-        if topic == f"{CLIENT_ID}/rele":
-            if msg == "on":
-                rele_estado = False
-            elif msg == "off":
-                rele_estado = True
-
-        if topic == f"{CLIENT_ID}/destello":
-            if msg == "on":
-                destello()
 
 async def destello():
     pin_led.value(True)
     await asyncio.sleep(200)
     pin_led.value(False)
     await asyncio.sleep(200)
-
-
 
 
 # Define configuration
